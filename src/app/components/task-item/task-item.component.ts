f613752e8a6811c7adbd5de9f91ee543a6845a15
Suskiiii <susannah.jarventausta@gmail.com>
2022-01-26 21:17:47 +0200

    Enable task deletion

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Task } from '../../Task';
 
@@ -9,9 +9,14 @@ import { Task } from '../../Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: Task | undefined;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: Task | undefined) {
+    this.onDeleteTask.emit(task);
+  }
 }
