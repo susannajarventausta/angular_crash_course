c256eaf92d024d12d478eb93b54f0f174045a27f
Suskiiii <susannah.jarventausta@gmail.com>
2022-01-27 11:08:32 +0200

    Enable task addition

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text: string | undefined;
+  day: string | undefined;
+  reminder: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task.');
+      return;
+    }
+
+    if (!this.day) {
+      alert('Please add a date.');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
